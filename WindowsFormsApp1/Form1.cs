10f68cbc104b2b1bcdee3d268ae6926538b39455
unisanta <unisanta@santacecilia.br>
2025-05-05 20:31:17 -0300

    feat: add atualizarLista method

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Model;
+using WindowsFormsApp1.Controller;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        private ControllerCliente cc = new ControllerCliente();
+
         public Form1()
         {
             InitializeComponent();
+            AtualizarLista();
+        }
+
+        private void AtualizarLista()
+        {
+            List<Cliente> clientes = cc.CarregarDados();
+            listBox1.Items.Clear();
+
+            foreach (Cliente cliente in clientes)
+            {
+                listBox1.Items.Add(cliente);
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
